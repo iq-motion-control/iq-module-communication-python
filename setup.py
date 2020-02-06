d492d46df1c23cd024c5e373429c7e962ae76711
IQ-Raf <raf@iq-control.com>
2020-02-06 16:25:57 -0500

    fixed setup.py and readme

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="iqmotion",
     packages=find_packages(),
@@ -7,6 +10,8 @@ setup(
     version="0.1.0",
     license="lgpl-3.0",
     description="Python libraries to talk to IQ Motion Control devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Raphael Van Hoffelen",
     author_email="raf@iq-control.com",
     url="https://github.com/iq-motion-control/iq-module-communication-python",
@@ -24,10 +29,9 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">=3.6",
 )
