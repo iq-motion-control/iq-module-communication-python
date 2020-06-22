c811a6989b47f6b3a29b2cdea40c7c61956d089f
IQ-Raf <raf@iq-control.com>
2020-06-22 14:44:50 -0400

    fix pipeline

--- ci_scripts/build_and_release_package.sh
+++ ci_scripts/build_and_release_package.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd ..
 
 python setup.py sdist  
-pip wheel --no-index --wheel-dir dist dist/*.tar.gz
+pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
 
 twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD  dist/*
