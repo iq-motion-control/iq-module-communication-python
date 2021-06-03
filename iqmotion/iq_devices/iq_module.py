8746a17b2957d7fbaee8128a78b84053f9cc5fc7
mparker25 <malik.parker@iq-control.com>
2021-06-03 11:29:42 -0400

    Restored the customIQModule to the iqmotion Library

--- iqmotion/iq_devices/iq_module.py
+++ iqmotion/iq_devices/iq_module.py
@@ -20,22 +20,28 @@ class IqModule:
     _DEFAULT_VELOCITY_CLIENT_ENTRY = ""
     _DEFAULT_VOLTS_CLIENT_ENTRY = ""
 
-    def __init__(self, com: Communicator, module_idn=0, clients_path=None):
+    def __init__(self, com: Communicator, module_idn=0, extra_clients=None, clients_path=None, module_file_path=None):
         
         self._client_dict = {}
         self._com = com
         self._module_idn = module_idn
 
         # Load the Module JSON (Base, Vertiq, Fortiq, etc...)
-        self._module_file_dict = IqModuleJsonParser.parse_default_modules(
-            self._MODULE_FILE_NAME
-        )
-        
-        # Deprecated: Add a custom module 
-        # self._module_file_dict = IqModuleJsonParser.parse_module(module_file_path)
+        if module_file_path is None:
+            self._module_file_dict = IqModuleJsonParser.parse_default_modules(
+                self._MODULE_FILE_NAME
+            )
+        # this only occurs from custom IQ Module
+        else:
+            self._module_file_dict = IqModuleJsonParser.parse_module(module_file_path)
 
         # Load the default clients from the Module JSON
         self._create_clients(self._module_file_dict)
+
+        # Load a list of json paths
+        if extra_clients is not None:
+            for extra_client in extra_clients:
+                self.add_client(extra_client)
 
         # Load the custom clients from the provided client_path?
         if clients_path is not None:
