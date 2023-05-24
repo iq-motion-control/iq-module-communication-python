e60e03b7536d73e5691664369e32f06baeeccade
Ben <ben.quan@vertiq.co>
2023-05-24 14:37:43 -0400

    removed custom_iq_module and pulsing_module for now

--- iqmotion/__init__.py
+++ iqmotion/__init__.py
@@ -1,5 +1,4 @@
 from iqmotion.iq_devices.base_iq_module import BaseIqModule
-from iqmotion.iq_devices.custom_iq_module import CustomIqModule
 
 # Soon to be deprecated: but the original way to access motor firmware API
 from iqmotion.iq_devices.speed_module import SpeedModule
@@ -11,7 +10,6 @@ from iqmotion.iq_devices.vertiq8108_module import Vertiq8108
 from iqmotion.iq_devices.vertiq4006_module import Vertiq4006
 from iqmotion.iq_devices.vertiq2306_module import Vertiq2306
 from iqmotion.iq_devices.fortiq_module import Fortiq
-from iqmotion.iq_devices.pulsing_module import PulsingModule
 
 from iqmotion.communication.communicator import Communicator
 from iqmotion.communication.serial_communicator import SerialCommunicator
