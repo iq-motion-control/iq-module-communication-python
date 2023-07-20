bcb839ca7e601e484cdb1162bdbc8aff4d10704b
Ben Quan <ben.quan@vertiq.co>
2023-07-20 22:05:39 +0000

    Merged in feature/test-github-submodule (pull request #72)
    update setup.py
    
    * update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setup(
     name="iqmotion",
     packages=["iqmotion", "client_files"],
