2c840ba1efbf880f1a5846c105535be3049495af
Ben <ben.quan@vertiq.co>
2023-07-21 12:55:02 -0400

    added comment in build_and_release_packagge.sh

--- ci_scripts/build_and_release_package.sh
+++ ci_scripts/build_and_release_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git submodule update --init
+git submodule update --init # required to initialize submodules in Bitbucket Pipeline
 python setup.py sdist  
 pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
 
