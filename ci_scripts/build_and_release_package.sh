fce88a6e849e92d6985ba635bcf75e014790c4c1
Ben Quan <ben.quan@vertiq.co>
2023-07-24 21:01:28 +0000

    Merged in development (pull request #80)
    Development
    
    * replaced packages field with find_packages() in setup.py
    
    * updated MANIFEST.in
    
    * updated client_files
    
    * Merged in bugfix/client_files_submodule (pull request #75)
    
    Bugfix/client files submodule
    * Merged in bugfix/client_files_submodule (pull request #77)
    
    testing fix
    
    * testing fix
    * Merged in bugfix/client_files_submodule (pull request #79)
    
    Bugfix/client files submodule
    
    * testing fix
    
    * added comment in build_and_release_packagge.sh
    
    * fix merge conflict
    
    * reformatted build_and_release_package.sh
    
    * updated client_files submodule
    
    * updated client_files submodule url to relative path
    
    * updated submodule
    * fix merge conflicts

--- ci_scripts/build_and_release_package.sh
+++ ci_scripts/build_and_release_package.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# required to initialize submodules in Bitbucket Pipeline
 git submodule update --init
-python setup.py sdist  
+
+python setup.py sdist
 pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
 
 twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD  dist/*
