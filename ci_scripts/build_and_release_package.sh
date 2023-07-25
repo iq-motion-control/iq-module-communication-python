5c9dc7102c9be848c2bdbe2e25db81e31dc32c18
Ben Quan <ben.quan@vertiq.co>
2023-07-25 22:44:13 +0000

    Merged in bugfix/client_files_submodule (pull request #91)
    Bugfix/client files submodule
    
    * removed git config stuff
    
    * Merge branch 'development' of bitbucket.org:iqcontrol/python_api into bugfix/client_files_submodule
    
    * Merge branch 'development' of bitbucket.org:iqcontrol/python_api into bugfix/client_files_submodule
    
    * testing fix for submodule path
    
    * Merge branch 'development' of bitbucket.org:iqcontrol/python_api into bugfix/client_files_submodule
    
    * fixed path
    
    * Merge branch 'development' of bitbucket.org:iqcontrol/python_api into bugfix/client_files_submodule
    
    * update .gitmodules relative path
    
    * Merge branch 'development' of bitbucket.org:iqcontrol/python_api into bugfix/client_files_submodule
    
    * undo overwrite of url in build_and_release_package.sh

--- ci_scripts/build_and_release_package.sh
+++ ci_scripts/build_and_release_package.sh
@@ -3,6 +3,7 @@
 # required to initialize submodules in Bitbucket Pipeline
 sed -i 's/url = \.\.\(.*\)/url = git@bitbucket.org:'"$BITBUCKET_WORKSPACE"'\1/g' ./.gitmodules
 git submodule update --init --recursive
+sed -i 's/url = git@bitbucket.org:'"$BITBUCKET_WORKSPACE"'/url = \.\./g' ./.gitmodules
 
 python setup.py sdist
 pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
